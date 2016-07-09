7fa5db7f06917f26c4479922e2c1365d7147e302
root <root@dell>
2016-07-09 20:13:30 -0300

    chance format message for new TATU format

--- src/MQTTOperations.java
+++ src/MQTTOperations.java
@@ -8,12 +8,12 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+import org.json.JSONObject;
 
+public class MQTTOperations implements MqttCallback {
 
-public class MQTTOperations implements MqttCallback{
-	
-	public static String topicPrefix = "dev/"; 
-	
+	public static String topicPrefix = "dev/";
+
 	private String brokerUrl;
 	private String brokerPort;
 	private String serverId;
@@ -22,32 +22,34 @@ public class MQTTOperations implements MqttCallback{
 	private MqttClient subscriber;
 	private MqttClient publisher;
 	private List<VirtualDevice> devices;
-	
+
 	public MQTTOperations(String brokerUrl, String brokerPort, String serverId,
 			String username, String password, List<VirtualDevice> devices) {
-		
+
 		MqttConnectOptions connOpt = new MqttConnectOptions();
-		
+
 		this.brokerUrl = brokerUrl;
 		this.brokerPort = brokerPort;
 		this.serverId = serverId;
 		this.username = username;
 		this.password = password;
 		this.devices = devices;
-		
+
 		try {
-			if(!this.username.isEmpty())
+			if (!this.username.isEmpty())
 				connOpt.setUserName(this.username);
-			if(!this.password.isEmpty())
+			if (!this.password.isEmpty())
 				connOpt.setPassword(this.password.toCharArray());
-			
-			this.subscriber = new MqttClient(this.brokerUrl + ":" + this.brokerPort, this.serverId);
+
+			this.subscriber = new MqttClient(this.brokerUrl + ":"
+					+ this.brokerPort, this.serverId);
 			this.subscriber.setCallback(this);
 			this.subscriber.connect(connOpt);
 			subscribeDevices(1);
 			System.out.println("Topic devices subscribed");
-			
-			this.publisher = new MqttClient(this.brokerUrl + ":" + this.brokerPort, this.serverId + "_pub");
+
+			this.publisher = new MqttClient(this.brokerUrl + ":"
+					+ this.brokerPort, this.serverId + "_pub");
 			this.publisher.setCallback(this);
 			this.publisher.connect(connOpt);
 		} catch (MqttException e) {
@@ -55,23 +57,25 @@ public class MQTTOperations implements MqttCallback{
 			e.printStackTrace();
 			System.exit(-1);
 		}
-		
+
 	}
-	
+
 	@Override
-    public void connectionLost(Throwable cause) {
+	public void connectionLost(Throwable cause) {
 		try {
 			MqttConnectOptions connOpt = new MqttConnectOptions();
-			
-			this.subscriber = new MqttClient(this.brokerUrl + ":" + this.brokerPort, this.serverId);
+
+			this.subscriber = new MqttClient(this.brokerUrl + ":"
+					+ this.brokerPort, this.serverId);
 			this.subscriber.setCallback(this);
 			this.subscriber.connect(connOpt);
 			subscribeDevices(1);
-			
-			this.publisher = new MqttClient(this.brokerUrl + ":" + this.brokerPort, this.serverId + "_publisher");
+
+			this.publisher = new MqttClient(this.brokerUrl + ":"
+					+ this.brokerPort, this.serverId + "_publisher");
 			this.publisher.setCallback(this);
 			this.publisher.connect(connOpt);
-			
+
 			System.out.println("Topic devices subscribed");
 		} catch (MqttException e) {
 			// TODO Auto-generated catch block
@@ -79,81 +83,87 @@ public class MQTTOperations implements MqttCallback{
 			System.exit(-1);
 		}
 
-    }
+	}
 
-    @Override
-    public void messageArrived(String topic, MqttMessage message) throws Exception {
-    	System.out.println("-------------------------------------------------");
+	@Override
+	public void messageArrived(String topic, MqttMessage message)
+			throws Exception {
+		System.out.println("-------------------------------------------------");
 		System.out.println("| Topic:" + topic);
 		System.out.println("| Message: " + new String(message.getPayload()));
-		System.out.println("-------------------------------------------------"); 
+		System.out.println("-------------------------------------------------");
+		String messageContent = new String(message.getPayload());
+
+		try {
+			JSONObject json = new JSONObject(messageContent);
+			
+			if (json.get("CODE").toString().contentEquals("GET")) {
+				VirtualDevice device = getDeviceOfTopic(topic);
+				MqttMessage answer = buildAnwserDevice(topic, device, json);
+				this.publisher.publish(topic + "/RES", answer);
+			}
+		} catch (org.json.JSONException e) {
+
+		}
 		
-    	String messageContent = new String(message.getPayload());
-    	if (messageContent.substring(0, 3).contentEquals(new String("GET"))){
-    		VirtualDevice device = getDeviceOfTopic(topic);
-    		MqttMessage answer = buildAnwserDevice(topic, device,message);
-    		this.publisher.publish(topic + "/RES", answer);
-		}   
-    }
 
-    @Override
-    public void deliveryComplete(IMqttDeliveryToken token) {
-        // TODO Auto-generated method stub
+	}
 
-    }
-    
-    private void subscribeDevices(int qos){
-    	for(int i=0;i<this.devices.size(); i++){
-    		try {
-    			String topic = topicPrefix + devices.get(i).getName() + "/#";
+	@Override
+	public void deliveryComplete(IMqttDeliveryToken token) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void subscribeDevices(int qos) {
+		for (int i = 0; i < this.devices.size(); i++) {
+			try {
+				String topic = topicPrefix + devices.get(i).getName() + "/#";
 				this.subscriber.subscribe(topic, qos);
 			} catch (MqttException e) {
 				e.printStackTrace();
 				System.exit(-1);
 			}
-    	}
-    }
-    
-    private VirtualDevice getDeviceOfTopic(String topic){
-    	String deviceName = topic.split("/")[1];
-    	for(int i=0;i<this.devices.size(); i++){
-    		if(this.devices.get(i).getName().contentEquals(deviceName))
-    			return(this.devices.get(i));
-    	}
-        return null;
-    }
-
-	    private MqttMessage buildAnwserDevice(String topic, VirtualDevice device, MqttMessage message){
-	    	Random randomGenerator = new Random();
-			MqttMessage answer = new MqttMessage();
-			
-			String messageContent = new String(message.getPayload());
-			String type = messageContent.split(" ")[1];
-			String sensorName = messageContent.split(" ")[2];
-			
-			VirtualSensor sensor = device.getSensor(sensorName);
-			int i = randomGenerator.nextInt(sensor.getValues().size());
-			Object value = sensor.getValues().get(i);
-			String msg = "POST "  + device.getName()+ ":{\"HEADER\":{\"name\":\"" + sensor.getDevice().getName() + "\"},\"BODY\":{\"" + sensor.getName() + "\":"; //<resposta>}}"
-			if(type.contentEquals("INFO")){
-				msg = msg + "\"" + value + "\"";
-			} else if(type.contentEquals("VALUE")) {
-				msg = msg + value;
-			}
-			else if(type.contentEquals("STATE")){
-				if(((String)value).contentEquals("true")){
-					msg = msg + "\"T\"";
-				}else{
-					msg = msg + "\"F\"";
-				}
-			}
-			msg = msg + "}}";
-			answer.setPayload(msg.getBytes());
-			return answer;
 		}
-    
-    
-	public void disconnect(){
+	}
+
+	private VirtualDevice getDeviceOfTopic(String topic) {
+		String deviceName = topic.split("/")[1];
+		for (int i = 0; i < this.devices.size(); i++) {
+			if (this.devices.get(i).getName().contentEquals(deviceName))
+				return (this.devices.get(i));
+		}
+		return null;
+	}
+
+	private MqttMessage buildAnwserDevice(String topic, VirtualDevice device,
+			JSONObject message) {
+		Random randomGenerator = new Random();
+		MqttMessage answer = new MqttMessage();
+		
+		// {"CODE":"GET","DATA":"INFO","VAR":"temp"}
+		
+		String type = message.getString("DATA");
+		String sensorName = message.getString("VAR");
+
+		VirtualSensor sensor = device.getSensor(sensorName);
+		int i = randomGenerator.nextInt(sensor.getValues().size());
+		Object value = sensor.getValues().get(i);
+
+		JSONObject response = new JSONObject();
+		JSONObject header = new JSONObject();
+		JSONObject body = new JSONObject();
+		header.put("NAME", sensor.getDevice().getName());
+		body.put(sensor.getName(), value);
+		response.put("CODE", "POST");
+		response.put("HEADER", header);
+		response.put("BODY", body);
+
+		answer.setPayload(response.toString().getBytes());
+		return answer;
+	}
+
+	public void disconnect() {
 		try {
 			this.subscriber.disconnect();
 			this.publisher.disconnect();
@@ -161,21 +171,6 @@ public class MQTTOperations implements MqttCallback{
 			e.printStackTrace();
 			System.exit(-1);
 		}
-		
-	}
-	
-	public void test(){
-		MqttMessage message = new MqttMessage();
-	    message.setPayload("I'm alive".getBytes());
-	    try {
-			this.subscriber.publish("pahodemo/test", message);
-		} catch (MqttPersistenceException e) {
-			e.printStackTrace();
-		} catch (MqttException e) {
-			e.printStackTrace();
-		}
-	}
-	
 
-	
+	}
 }
